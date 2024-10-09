0927dba01279bc21a9e03c27e8eb0d1788b77a7c
Kiera ODonnell <klo30@duke.edu>
2024-10-09 12:44:56 -0400

    Creating a Venn Diagram of literature topics

--- LitReview.R
+++ LitReview.R
@@ -2,22 +2,36 @@
 # KLO 
 # 10/09/24
 
+library(VennDiagram)
+library(eulerr)
+
 # Would love to figure out how to use the Scopus and WoS API here...
 
 
 
 # Venn Diagrams using Scopus searches (manuel searches for now)
 
-VennAll <- data.frame(Group = c("Climate Change", "Sea Level Rise", "Saltwater Intrusion", 
-                                "Climate Change & Sea Level Rise", "Climate Change & Saltwater Intrusion", "Sea Level Rise & Saltwater Intrusion", "Climate Change & Sea Level Rise & Saltwater Intrusion"),
-                      All = c(462488, 23866, 2568,
-                              9281, 485, 489, 267))
+VennAll <- c("Climate Change" = 462488, "Sea Level Rise" = 23866, "Saltwater Intrusion" = 2568, 
+             "Climate Change&Sea Level Rise" = 9281, "Climate Change&Saltwater Intrusion" = 485, "Sea Level Rise&Saltwater Intrusion" = 489,
+             "Climate Change&Sea Level Rise&Saltwater Intrusion" = 267)
 
-VennPeople <- data.frame(Group = c("Climate Change", "Sea Level Rise", "Saltwater Intrusion", 
-                                   "Climate Change & Sea Level Rise", "Climate Change & Saltwater Intrusion", "Sea Level Rise & Saltwater Intrusion", "Climate Change & Sea Level Rise & Saltwater Intrusion"),
-                         All = c(129451, 5109, 309,
-                                 2640, 98, 67, 48))
+VennAll <- c("Climate Change" = 462488, "Sea Level Rise" = 23866, "Saltwater Intrusion" = 2568, 
+             "Climate Change&Sea Level Rise" = 9281, "Climate Change&Saltwater Intrusion" = 485, "Sea Level Rise&Saltwater Intrusion" = 489,
+             "Climate Change&Sea Level Rise&Saltwater Intrusion" = 267,
+             "People1&Climate Change" = 129451, "People2&Sea Level Rise" = 5109, "People3&Saltwater Intrusion" = 309)
 
+
+VennPeople <- c("ClimateChange" = 129451, "SeaLevelRise" = 5109, "SaltwaterIntrusion" = 309, 
+                "ClimateChange&SeaLevelRise" = 2640, "ClimateChange&SaltwaterIntrusion" = 98, "SeaLevelRise&SaltwaterIntrusion" = 67,
+                "ClimateChange&SeaLevelRise&SaltwaterIntrusion" = 48)
+  
+
+fit1 <- euler(VennAll)
+
+plot(fit1, fill = c("#61D04F", "#2297E6", "#F5C710"), alpha = 0.6, legend = TRUE, quantities = TRUE)
+
+fit2 <- euler(VennPeople)
+plot(fit2, fill = c("#61D04F", "#2297E6", "#F5C710"), alpha = 0.6, legend = TRUE, quantities = TRUE)
 
 # Since we didn't use Scopus API in the bass connections, upload the data here...
 
