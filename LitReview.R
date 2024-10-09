f2867609e5f5482eb06071dac352cba3e3c67a9c
Kiera ODonnell <klo30@duke.edu>
2024-10-09 11:39:03 -0400

    Setting up the project and outline of the code

---
+++ LitReview.R
@@ -0,0 +1,26 @@
+# SWISLR Literature Review Codes
+# KLO 
+# 10/09/24
+
+# Would love to figure out how to use the Scopus and WoS API here...
+
+
+
+# Venn Diagrams using Scopus searches (manuel searches for now)
+
+VennAll <- data.frame(Group = c("Climate Change", "Sea Level Rise", "Saltwater Intrusion", 
+                                "Climate Change & Sea Level Rise", "Climate Change & Saltwater Intrusion", "Sea Level Rise & Saltwater Intrusion", "Climate Change & Sea Level Rise & Saltwater Intrusion"),
+                      All = c(462488, 23866, 2568,
+                              9281, 485, 489, 267))
+
+VennPeople <- data.frame(Group = c("Climate Change", "Sea Level Rise", "Saltwater Intrusion", 
+                                   "Climate Change & Sea Level Rise", "Climate Change & Saltwater Intrusion", "Sea Level Rise & Saltwater Intrusion", "Climate Change & Sea Level Rise & Saltwater Intrusion"),
+                         All = c(129451, 5109, 309,
+                                 2640, 98, 67, 48))
+
+
+# Since we didn't use Scopus API in the bass connections, upload the data here...
+
+
+
+
